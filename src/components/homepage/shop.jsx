f6ac48cde70f6aca891c0741447b5c7e48d893d0
am-otunba <amosunolawale00@gmail.com>
2026-04-30 17:57:40 +0100

    Update shop Bumpa links

--- src/components/homepage/shop.jsx
+++ src/components/homepage/shop.jsx
@@ -3,16 +3,16 @@
 import Image from "next/image";
 
 const products = [
-{ id:1, name:"Black T-Shirt", price:"₦45,000", image:"/Black T-shirt.png", link:"https://thelayconstore.bumpa.shop/products/ksmw-t-shirt/4499475?location=313151"},
-{ id:2, name:"White T-Shirt", price:"₦45,000", image:"/White T-shirt.png", link:"https://thelayconstore.bumpa.shop/products/ksmw-t-shirt/4499475?location=313151"},
-{ id:3, name:"Black Hoodie", price:"₦55,000", image:"/Black Hoodie.png", link:"https://thelayconstore.bumpa.shop/products/ksmw-hoodie/4503053?location=313151"},
-{ id:4, name:"White Hoodie", price:"₦55,000", image:"/White Hoodie.png", link:"https://thelayconstore.bumpa.shop/products/ksmw-hoodie/4503053?location=313151"},
-{ id:5, name:"Black crop-top", price:"₦30,000", image:"/Black Crop-top.png", link:"https://thelayconstore.bumpa.shop/products/ksmw-crop-top/4501094?location=313151"},
-{ id:6, name:"White crop-top", price:"₦30,000", image:"/White Crop-top.png", link:"https://thelayconstore.bumpa.shop/products/ksmw-crop-top/4501094?location=313151"},
-{ id:7, name:"Double-Layered Sleeve Shirt", price:"₦55,000", image:"/Double Layered Sleeve Shirt.png", link:"https://thelayconstore.bumpa.shop/products/ksmw-double-layered-sleeve-shirt/4503022?location=313151"},
-{ id:8, name:"Black Tank", price:"₦35,000", image:"/Black Tank-top.png", link:"https://thelayconstore.bumpa.shop/products/ksmw-tank-top/4501176?location=313151"},
-{ id:9, name:"White Tank", price:"₦35,000", image:"/White Tank-top.png", link:"https://thelayconstore.bumpa.shop/products/ksmw-tank-top/4501176?location=313151"},
-{ id:10, name:"The Listening Experience Kampala (5 Yards)", price:"₦20,000", image:"/Kampala.png", link:"https://thelayconstore.bumpa.shop/products/ksmw-kampala/4504223?location=313151"}
+{ id:1, name:"Black T-Shirt", price:"₦45,000", image:"/Black T-shirt.png", link:"https://thekashimawoostore.bumpa.shop/products/black-tshirt/4720455?location=325597"},
+{ id:2, name:"White T-Shirt", price:"₦45,000", image:"/White T-shirt.png", link:"https://thekashimawoostore.bumpa.shop/products/white-t-shirt/4720471?location=325597"},
+{ id:3, name:"Black Hoodie", price:"₦55,000", image:"/Black Hoodie.png", link:"https://thekashimawoostore.bumpa.shop/products/black-hoodie/4720477?location=325597"},
+{ id:4, name:"White Hoodie", price:"₦55,000", image:"/White Hoodie.png", link:"https://thekashimawoostore.bumpa.shop/products/white-hoodie/4720482?location=325597"},
+{ id:5, name:"Black crop-top", price:"₦30,000", image:"/Black Crop-top.png", link:"https://thekashimawoostore.bumpa.shop/products/crop-top/4720498?location=325597"},
+{ id:6, name:"White crop-top", price:"₦30,000", image:"/White Crop-top.png", link:"https://thekashimawoostore.bumpa.shop/products/crop-top/4720507?location=325597"},
+{ id:7, name:"Double-Layered Sleeve Shirt", price:"₦55,000", image:"/Double Layered Sleeve Shirt.png", link:"https://thekashimawoostore.bumpa.shop/products/double-layered-sleeve-shirt/4720517?location=325597"},
+{ id:8, name:"Black Tank", price:"₦35,000", image:"/Black Tank-top.png", link:"https://thekashimawoostore.bumpa.shop/products/black-tank/4720530?location=325597"},
+{ id:9, name:"White Tank", price:"₦35,000", image:"/White Tank-top.png", link:"https://thekashimawoostore.bumpa.shop/products/white-tank/4720542?location=325597"},
+{ id:10, name:"The Listening Experience Kampala (5 Yards)", price:"₦20,000", image:"/Kampala.png", link:"https://thekashimawoostore.bumpa.shop/products/the-listening-experience-kampala/4720572?location=325597"}
 ];
 
 const Shop = () => {
